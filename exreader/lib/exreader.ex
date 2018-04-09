94f925cdad8bd0c210a57c81c3828067d272546f
Pedro Hernández <dsilmood@gmail.com>
2018-04-09 11:28:33 -0500

    First commit

---
+++ exreader/lib/exreader.ex
@@ -0,0 +1,18 @@
+defmodule Exreader do
+  @moduledoc """
+  Documentation for Exreader.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exreader.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ exreader/test/exreader_test.exs
@@ -0,0 +1,8 @@
+defmodule ExreaderTest do
+  use ExUnit.Case
+  doctest Exreader
+
+  test "greets the world" do
+    assert Exreader.hello() == :world
+  end
+end
