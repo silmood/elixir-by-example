ebda9dc17bf29c39bd0a01cb1b7c91c793f16f41
Pedro Hernández <pedro@bunsan.io>
2018-05-03 14:45:51 -0500

    🚀 Servy project was created

---
+++ servy/lib/servy.ex
@@ -0,0 +1,18 @@
+defmodule Servy do
+  @moduledoc """
+  Documentation for Servy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Servy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
